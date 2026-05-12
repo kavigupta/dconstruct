59077c03f24d6d9dc0f83c9c6a61c9e3d5e73232
Kavi Gupta <kavi@berkeley.edu>
2026-05-12 15:18:46 -0400

    allow more dconstruct versions (#1)
    * allow more dconstruct versions
    
    * whatever

--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ setuptools.setup(
     url="https://github.com/kavigupta/dconstruct",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.7",
     install_requires=[],
     # documentation
     project_urls={
